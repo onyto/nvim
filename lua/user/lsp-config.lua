98146ea3d5810784db85ce5919008b9e6b06b5ea
onyto <onyto@tutanota.com>
2023-08-23 20:01:41 +0200

    Update lua_ls config

--- lua/user/lsp-config.lua
+++ lua/user/lsp-config.lua
@@ -81,7 +81,8 @@ require'lspconfig'.lua_ls.setup {
         },
         workspace = {
           -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
+          library = { vim.env.VIMRUNTIME },
+          checkThirdParty = false,
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = {
